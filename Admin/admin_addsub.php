7c153d6d51daffbb6e7ca3cf5566bef453a0749d
Tachrox <tarunchandra312@gmail.com>
2024-05-23 23:45:37 +0530

    Files Added

--- Admin/admin_addsub.php
+++ Admin/admin_addsub.php
@@ -2,9 +2,44 @@
 
 <head>
     <title>Subject</title>
+    <link rel="stylesheet" type="text/css" href="admin.css">
 </head>
 
 <body>
+<nav class="navbar">
+        <ul>
+            <li><a href="../main.html">HOME</a></li>
+            <li><a href="adminlogin.php">ADMIN</a></li>
+            <li><a href="../Teacher/teacherlogin.php">TEACHER</a></li>
+            <li><a href="../Student/studentlogin.php">STUDENT</a></li>
+        </ul>
+    </nav>
+    <div class="container">
+        <main class="main-content">
+            <div class="admin-options">
+                <div class="admin-option">
+                    <a href="admin_addsub.php">
+                        <button>ADD SUBJECT</button>
+                    </a>
+                </div>
+                <div class="admin-option">
+                    <a href="admin_addt.php">
+                        <button>ADD TEACHER</button>
+                    </a>
+                </div>
+                <div class="admin-option">
+                    <a href="admin_addst.php">
+                        <button>ADD STUDENT</button>
+                    </a>
+                </div>
+                <div class="admin-option">
+                    <a href="assignclass.php">
+                        <button>ASSIGN CLASS</button>
+                    </a>
+                </div>
+            </div>
+        </main>
+    </div>
     <center>
         <br><br><br><br><br><br>
         <h2>Deatils of Subject</h2>
@@ -85,7 +120,10 @@
         ?>
         </table>
     </center>
-
+    <footer class="footer">
+        <p>&copy; 2024 Student's Attendance Management System. All rights reserved.</p>
+        <p><a href="contact.html">Contact Us</a> | <a href="about.html">About</a></p>
+    </footer>
 </body>
 
 </html>

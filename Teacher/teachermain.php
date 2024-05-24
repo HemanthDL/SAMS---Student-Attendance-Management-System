333b42978f95dfb9cf478d6eb8c26419eba4d2d9
HemanthDL <hemanthdl2907@gmail.com>
2024-05-24 06:18:50 +0530

    Files Added

--- Teacher/teachermain.php
+++ Teacher/teachermain.php
@@ -1,53 +1,93 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="../Admin/admin.css">
+    <style>
+        .att-teas{
+            position:absolute;
+            bottom: 192px;
+            right: 550px;
+            z-index: 1;
+            display:flex;
+            justify-content:space-between;
+            align-items:center;
+            margin-bottom:25px;
+        }
+        .admin-option{
+            margin:25px;
+        }
+        .info{
+            font-size:larger;
+            display:flex;
+            align-items:center;
+            margin-right: auto;
+        }
+        .info img {
+            margin-left:15px;
+            margin-right: 50px; 
+            max-width: 100px; 
+            border-radius:50%;
+        }
+        h2{
+            position: absolute;
+            top:175px;
+            left:65px;
+        }
+    </style>
 </head>
+
 <body>
-    <h2><center>
-    <?php
-        include('../connection.php');
-        //include('teacherlogin.php');
-        session_start();
-        $tid=$_SESSION['tid'];
-        $sql="select * from Teacher where id='$tid'";
-        $res=$conn->query($sql);
-        $r=$res->fetch_assoc();
-        echo "Name : ".$r['name']."<br>Department : ".$r['dept']."<br>Degree : ".$r['degree']."<br>";
-        
-    ?>
-    <center></h2>
-    <br><br><br><br><br><br><br><br>
-    <h2><center>
-        <table>
-            <tr>
-                <th>
-                    <!-- <a href="markattendance.php?tid='$tid'&dept='$r['dept']'">TAKE ATTENDANCE</a> -->
-                    <?php
-                            $url="markattendance.php";
-                            $_SESSION['tid']=$tid;
-                            $_SESSION['dept']=$r['dept'];
-                            echo "<a href='$url'>MARK ATTENDANCE</a>";
-                    ?>
-                    <br><br><br>
-                </th>
-            </tr>
-            <tr>
-                <th>
-                    <a href="viewattendance1.php">VIEW ATTENDANCE</a>
-                    <br><br><br>
-                </th>
-            </tr>
-            <tr>
-                <th>
-                    <a href="../main.html">
-		                <input type="button" value="HOME">
-	                </a>
-                </th>
-            </tr>
-        </table>
-</center></h2>
+    <nav class="navbar">
+        <ul>
+            <li><a href="../main.html">HOME</a></li>
+            <li><a href="adminlogin.php">ADMIN</a></li>
+            <li><a href="../Teacher/teacherlogin.php">TEACHER</a></li>
+            <li><a href="../Student/studentlogin.php">STUDENT</a></li>
+        </ul>
+    </nav>
+    <h2>Teacher Deatils</h2>
+    <div class="container">
+        <div class='info'> 
+                <?php
+                    include('../connection.php');
+                    //include('teacherlogin.php');
+                    session_start();
+                    $tid=$_SESSION['tid'];
+                    $sql="select * from Teacher where id='$tid'";
+                    $res=$conn->query($sql);
+                    $r=$res->fetch_assoc();
+                    echo "<img src='https://thumbs.dreamstime.com/b/teacher-icon-flat-style-illustration-web-125576005.jpg' alt='Teacher Image'>"; // Add your image source here
+                    echo "<div>";
+                    echo "Name : ".$r['name']."<br>";
+                    echo "Department : ".$r['dept']."<br>";
+                    echo "Degree : ".$r['degree'];
+                    echo "</div>";
+                ?>
+        </div>
+    </div>
+    <div class="att-teas">
+            <div class="admin-option">
+                <?php
+                        $url="markattendance.php";
+                        $_SESSION['tid']=$tid;
+                        $_SESSION['dept']=$r['dept'];
+                        echo "<a href='$url'>
+                        <button>MARK ATTENDANCE</button></a>";
+                ?>
+            </div>
+            <div class="admin-option">
+                <a href="viewattendance1.php"><button>VIEW ATTENDANCE</button></a>
+            </div>
+    </div>
+    
+    <footer class="footer">
+        <p>&copy; 2024 Student's Attendance Management System. All rights reserved.</p>
+        <p><a href="contact.html">Contact Us</a> | <a href="about.html">About</a></p>
+    </footer>
 </body>
+
 </html>
